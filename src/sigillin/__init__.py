81b3d369d5887b2100de759a588e4fc74e4111bf
GenesisAeon <joberoemer@gmail.com>
2026-03-26 20:36:04 +0100

    chore: bump to v0.3.1 (fix PyPI duplicate)

--- src/sigillin/__init__.py
+++ src/sigillin/__init__.py
@@ -1,6 +1,7 @@
-"""sigillin – the poetic-symbolic interface layer of the GenesisAeon stack."""
+﻿"""sigillin â€“ the poetic-symbolic interface layer of the GenesisAeon stack."""
 
-__version__ = "0.1.0"
+__version__ = "0.3.1"
 __all__ = ["Sigil"]
 
 from .core import Sigil
+
